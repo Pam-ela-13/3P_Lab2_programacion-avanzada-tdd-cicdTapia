cf82dfbe5b68f6f90867efb1c3591b371a29acd1
Pam-ela-13 <pntapia1@espe.edu.ec>
2026-07-16 22:18:02 -0500

    refactor: limpia estructura de PedidoController

--- src/main/java/com/tapia/lab1/controller/PedidoController.java
+++ src/main/java/com/tapia/lab1/controller/PedidoController.java
@@ -3,7 +3,11 @@ package com.tapia.lab1.controller;
 import com.tapia.lab1.model.Pedido;
 import com.tapia.lab1.service.PedidoService;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +19,17 @@ public class PedidoController {
     private final PedidoService pedidoService;
 
     public PedidoController(PedidoService pedidoService) {
-
         this.pedidoService = pedidoService;
     }
 
     @GetMapping
     public List<Pedido> porEstado(@RequestParam String estado) {
-
         return pedidoService.obtenerPorEstado(estado);
+    }
+
+    @PostMapping
+    public ResponseEntity<Pedido> crear(@RequestBody Pedido pedido) {
+        Pedido creado = pedidoService.crearPedido(pedido);
+        return ResponseEntity.status(HttpStatus.CREATED).body(creado);
     }
 }
